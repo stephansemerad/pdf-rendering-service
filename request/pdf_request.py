3e833b7fd14029da52fc3728a3b0e2c786ce6503
root <stephan.semerad@gmail.com>
2021-01-09 20:46:45 +0000

    fixing request to not be local

--- request/pdf_request.py
+++ request/pdf_request.py
@@ -3,14 +3,14 @@ API_KEY = 'y42WTaddY2pt7m90tqKW'
 
 # 1. POST /documents
 file_path   = os.path.join(os.getcwd(), 'sample_1.pdf')
-API_URL     = 'http://127.0.0.1:8000/documents'
+API_URL     = 'http://138.68.102.253:8080/documents'
 content     =  open(file_path, 'r').read()
 response    = requests.post(API_URL, headers={'api-key': API_KEY}, data=content).json()
 print('response: ', response)
 
 
 file_path   = os.path.join(os.getcwd(), 'sample_2.pdf')
-API_URL     = 'http://127.0.0.1:8000/documents'
+API_URL     = 'http://138.68.102.253:8080/documents'
 content     =  open(file_path).read()
 response    = requests.post(API_URL, headers={'api-key': API_KEY}, data=content).json()
 print('response: ', response)
@@ -33,7 +33,7 @@ images = convert_from_bytes(open(file_path, 'rb').read())
 
 
 file_path   = os.path.join(os.getcwd(), 'sample_1.txt')
-API_URL     = 'http://127.0.0.1:8000/documents'
+API_URL     = 'http://138.68.102.253:8080/documents'
 content     =  open(file_path, 'r').read()
 response    = requests.post(API_URL, headers={'api-key': API_KEY}, data=content).json()
 print('response: ', response)
@@ -42,7 +42,7 @@ print('response: ', response)
 
 # 2. GET /documents/<DOCUMENT_ID>
 DOCUMENT_ID = 'ABC'
-API_URL = 'http://127.0.0.1:8000/documents/' + DOCUMENT_ID
+API_URL = 'http://138.68.102.253:8080/documents/' + DOCUMENT_ID
 response = requests.get(API_URL, headers={'api-key': API_KEY}).json()
 print('response: ', response)
 
@@ -50,7 +50,7 @@ print('response: ', response)
 # 3. GET  /documents/<DOCUMENT_ID>/pages/
 DOCUMENT_ID = 'ABC'
 PAGE        = '1'
-API_URL = 'http://127.0.0.1:8000/documents/pages/' + DOCUMENT_ID + '/' + pages
+API_URL = 'http://138.68.102.253:8080/documents/pages/' + DOCUMENT_ID + '/' + pages
 response = requests.get(API_URL, headers={'api-key': API_KEY}).json()
 print('response: ', response)
 
